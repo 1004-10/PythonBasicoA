1d1a236d3f8ef551554d74e8491c4fce2e5b0abd
David <davidmatias10@outlook.es>
2021-02-11 23:14:36 -0600

    Hoja 1 ejercicios
    Hoja de trabajo 1 python basico 2021 seccion A

---
+++ hoja1ejercicio2.py
@@ -0,0 +1,16 @@
+a = 3473
+b = 11
+
+def es_primo(num) :
+
+    contador = 0
+
+    for i in range(1, num+1) :
+        if num % i == 0 :
+            contador += 1
+
+        if contador == 2:
+            return True
+        else:
+            return False
+print(es_primo(27))                    
